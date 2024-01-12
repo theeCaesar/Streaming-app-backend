e7a5323a5706fbe984bb0eb3fb5317c979cbd6b4
theeCaesar <workthecaesar@gmail.com>
2024-01-12 21:09:07 +0300

    add chat messages model

--- models/chatsModel.js
+++ models/chatsModel.js
@@ -1,18 +1,35 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const chatsSchema = new mongoose.Schema(
-//   {
-//     message: {
-//       type: String,
-//       required: [true, 'chat must have a message'],
-//     },
-//     ,
-//   },
-//   {
-//     toJSON: { getters: true, virtuals: true },
-//     toObject: { getters: true, virtuals: true },
-//     id: false,
-//   },
-// );
+const chatMessagesSchema = new mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: [true, 'chat must have a message'],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: [true, 'message must belong to a user'],
+    },
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'rooms',
+      required: [true, 'message must belong to a room'],
+    },
+    movie: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'movies',
+      required: [true, 'message must belong to a movie'],
+    },
+    timestamp: Number, //in seconds
+  },
+  {
+    toJSON: { getters: true, virtuals: true },
+    toObject: { getters: true, virtuals: true },
+    id: false,
+  },
+);
 
-// const Movies = mongoose.model('movies', movieSchema);
+const ChatMessages = mongoose.model('chatMessages', movieSchema);
+
+module.exports = ChatMessages;
